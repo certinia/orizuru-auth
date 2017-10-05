5856db1b7e1f80232fa3081252268822ef943a14
Stephen Fry <sfry@financialforce.com>
2017-10-05 10:16:43 +0100

    Add jsdoc.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 'use strict';
 
+/**
+ * Nozomi Auth module.
+ * @module
+ * @see module:openid/middleware
+ * @see module:openid/grant
+ */
+
 module.exports = {
+	/**
+	 * The express middlewares module.
+	 */
 	middleware: require('./openid/middleware'),
+	/**
+	 * The token granter module.
+	 */
 	grant: require('./openid/grant')
 };

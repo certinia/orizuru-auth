f10d24ab03dd45a08ae0366713e3fbf39c40633b
Matthew Wood <mwood@financialforce.com>
2018-08-30 12:35:35 +0100

    Remove extra line

--- test/revocation/revoke.test.ts
+++ test/revocation/revoke.test.ts
@@ -24,7 +24,6 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';

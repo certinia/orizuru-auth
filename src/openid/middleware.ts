5a5a693f8a4c02ba9c8515c0df43c09a9c6bdead
Peter Wright <pwright@financialforce.com>
2019-01-21 12:23:00 +0000

    Refactor event names to match legacy sf-auth

--- src/openid/middleware.ts
+++ src/openid/middleware.ts
@@ -51,11 +51,11 @@ const BEARER_PREFIX: string = 'Bearer ';
  */
 const HTTP_AUTHORIZATION_HEADER: string = 'Authorization';
 
-export const DENIED_EVENT = 'denied';
+export const EVENT_DENIED = 'denied';
 
-export const GRANT_CHECKED_EVENT = 'grant_checked';
+export const EVENT_GRANT_CHECKED = 'grant_checked';
 
-export const TOKEN_VALIDATED_EVENT = 'token_validated';
+export const EVENT_TOKEN_VALIDATED = 'token_validated';
 
 /**
  * @private
@@ -116,7 +116,7 @@ function setUserOnRequest(req: Request, userInfo: UserInfo) {
 	orizuru.user = user;
 	req.orizuru = orizuru;
 
-	middleware.emitter.emit(TOKEN_VALIDATED_EVENT, `Token validated for: ${req.ip}`);
+	middleware.emitter.emit(EVENT_TOKEN_VALIDATED, `Token validated for: ${req.ip}`);
 
 }
 
@@ -129,7 +129,7 @@ function setGrant(req: Request) {
 
 		(req.orizuru as Orizuru.Context).grantChecked = true;
 
-		middleware.emitter.emit(GRANT_CHECKED_EVENT, `Grant checked for: ${req.ip}`);
+		middleware.emitter.emit(EVENT_GRANT_CHECKED, `Grant checked for: ${req.ip}`);
 
 		return undefined;
 
@@ -144,7 +144,7 @@ function fail(req: Request, res: Response) {
 
 	return (error: Error) => {
 
-		middleware.emitter.emit(DENIED_EVENT, `Access denied to: ${req ? req.ip ? req.ip : 'unknown' : 'unknown'}, error: ${error.message}.`);
+		middleware.emitter.emit(EVENT_DENIED, `Access denied to: ${req ? req.ip ? req.ip : 'unknown' : 'unknown'}, error: ${error.message}.`);
 
 		res.sendStatus(401);
 

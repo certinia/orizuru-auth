d368b21bd3cfd2cc868d06428b0385f2aa618c91
Matthew Wood <mwood@financialforce.com>
2019-03-04 10:18:17 +0000

    Add the AccessTokenResponse interface

--- src/index.ts
+++ src/index.ts
@@ -83,6 +83,90 @@ declare global {
 }
 
 /**
+ * The access token response.
+ *
+ * @example
+ * ```json
+ *
+ * {
+ * 	"id": "https://login.salesforce.com/id/00Dx0000000BV7z/005x00000012Q9P",
+ * 	"issued_at": "1278448101416",
+ * 	"refresh_token": "5Aep8614iLM.Dq661ePDmPEgaAW9Oh_L3JKkDpB4xReb54_pZebnUG0h6Sb4KUVDpNtWEofWM39yg==",
+ * 	"instance_url": "https://yourInstance.salesforce.com/",
+ * 	"signature": "CMJ4l+CCaPQiKjoOEwEig9H4wqhpuLSk4J2urAe+fVg=",
+ * 	"access_token": "00Dx0000000BV7z!AR8AQP0jITN80ESEsj5EbaZTFG0RNBaT1cyWk7TrqoDjoNIWQ2ME_sTZzBjfmOE6zMHq6y8PIW4eWze9JksNEkWUl.Cju7m4",
+ * 	"token_type": "Bearer",
+ * 	"scope": "id api refresh_token"
+ * }
+ * ```
+ */
+export interface AccessTokenResponse {
+
+	/**
+	 * Salesforce session ID that can be used with the web services API.
+	 */
+	access_token: string;
+
+	/**
+	 * Value is Bearer for all responses that include an access token.
+	 */
+	token_type: string;
+
+	/**
+	 * Salesforce value conforming to the OpenID Connect specifications.
+	 * This parameter is returned only if the scope parameter includes openid.
+	 */
+	id_token?: null | string | OpenIDToken | OpenIDTokenWithStandardClaims;
+
+	/**
+	 * Token that can be used in the future to obtain new access tokens (sessions).
+	 * This value is a secret. Treat it like the user’s password, and use appropriate measures to protect it.
+	 * This parameter is returned only if your connected app is set up with a scope of at least refresh_token.
+	 */
+	refresh_token?: string;
+
+	/**
+	 * A URL indicating the instance of the user’s org. For example: https://yourInstance.salesforce.com/.
+	 */
+	instance_url: string;
+
+	/**
+	 * Identity URL that can be used to both identify the user and query for more information about the user.See Identity URLs.
+	 */
+	id: string;
+
+	/**
+	 * This parameter is returned if the scope parameter includes openid.
+	 */
+	scope?: string;
+
+	/**
+	 * If the user is a member of a Salesforce community, the community URL is provided.
+	 */
+	sfdc_community_url?: string;
+
+	/**
+	 * If the user is a member of a Salesforce community, the user’s community ID is provided.
+	 */
+	sfdc_community_id?: string;
+
+	/**
+	 * Base64-encoded HMAC-SHA256 signature signed with the client_secret (private key) containing the
+	 * concatenated ID and issued_at. Used to verify that the identity URL hasn’t changed since the
+	 * server sent it.
+	 */
+	signature: string;
+
+	/**
+	 * When the signature was created.
+	 */
+	issued_at: string;
+
+	userInfo?: UserInfo;
+
+}
+
+/**
  * A token that conforms to the [OpenID specification](https://openid.net/specs/openid-connect-basic-1_0-28.html#id_token).
  *
  * It is returned in the `id_token` field of the `AccessTokenResponse`.

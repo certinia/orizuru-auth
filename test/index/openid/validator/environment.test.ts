fb7b4741a92fa1a802c22478706e5786e99c09e1
Matthew Wood <mwood@financialforce.com>
2019-03-04 10:42:57 +0000

    Add the environment validator and tests

---
+++ test/index/openid/validator/environment.test.ts
@@ -0,0 +1,183 @@
+/**
+ * Copyright (c) 2017-2019, FinancialForce.com, inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ *   are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import chai from 'chai';
+
+import { Environment } from '../../../../src';
+
+import { validate } from '../../../../src/index/openid/validator/environment';
+
+const expect = chai.expect;
+
+describe('validator/environment', () => {
+
+	let env: Environment;
+
+	beforeEach(() => {
+		env = {
+			jwtSigningKey: 'test',
+			openidClientId: 'test',
+			openidClientSecret: 'test',
+			openidHTTPTimeout: 4001,
+			openidIssuerURI: 'test'
+		};
+	});
+
+	describe('should reject', () => {
+
+		it('if the environment is undefined', () => {
+
+			// Given
+			delete env.jwtSigningKey;
+
+			// When
+			// Then
+			expect(() => validate(undefined)).to.throw('Missing required object parameter.');
+
+		});
+
+		it('if jwtSigningKey is undefined', () => {
+
+			// Given
+			delete env.jwtSigningKey;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Missing required string parameter: jwtSigningKey.');
+
+		});
+
+		it('if jwtSigningKey is empty', () => {
+
+			// Given
+			env.jwtSigningKey = '';
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Invalid parameter: jwtSigningKey cannot be empty.');
+
+		});
+
+		it('if openidClientId is undefined', () => {
+
+			// Given
+			delete env.openidClientId;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Missing required string parameter: openidClientId.');
+
+		});
+
+		it('if openidClientId is empty', () => {
+
+			// Given
+			env.openidClientId = '';
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Invalid parameter: openidClientId cannot be empty.');
+
+		});
+
+		it('if openidClientSecret is undefined', () => {
+
+			// Given
+			delete env.openidClientSecret;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Missing required string parameter: openidClientSecret.');
+
+		});
+
+		it('if openidClientSecret is empty', () => {
+
+			// Given
+			env.openidClientSecret = '';
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Invalid parameter: openidClientSecret cannot be empty.');
+
+		});
+
+		it('if openidHTTPTimeout is undefined', () => {
+
+			// Given
+			delete env.openidHTTPTimeout;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Missing required number parameter: openidHTTPTimeout.');
+
+		});
+
+		it('if openidHTTPTimeout is not an integer', () => {
+
+			// Given
+			env.openidHTTPTimeout = 'test' as unknown as number;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Invalid parameter: openidHTTPTimeout is not a number.');
+
+		});
+
+		it('if openidIssuerURI is undefined', () => {
+
+			// Given
+			delete env.openidIssuerURI;
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Missing required string parameter: openidIssuerURI.');
+
+		});
+
+		it('if openidIssuerURI is empty', () => {
+
+			// Given
+			env.openidIssuerURI = '';
+
+			// When
+			// Then
+			expect(() => validate(env)).to.throw('Invalid parameter: openidIssuerURI cannot be empty.');
+
+		});
+
+	});
+
+	it('should resolve with env if env is ok', () => {
+
+		// Given
+		// When
+		// Then
+		expect(validate(env)).to.deep.equal(env);
+
+	});
+
+});

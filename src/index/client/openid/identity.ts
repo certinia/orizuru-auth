bd79ef106f0d393c32d320e6f843cbb8e66443db
Matthew Wood <mwood@financialforce.com>
2019-03-11 10:44:09 +0000

    Add the OpenId Identity checks and tests The Salesforce related components will be separated out

---
+++ src/index/client/openid/identity.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2019, FinancialForce.com, inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ *   are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @module client/openid/identity
+ */
+
+import { decode } from 'jsonwebtoken';
+
+import { AccessTokenResponse } from '../oauth2';
+import { OpenIDAccessTokenResponse, OpenIDToken, OpenIDTokenWithStandardClaims } from '../openid';
+
+/**
+ * Decode the `id_token` from an access token response.
+ *
+ * This does not verify the token.
+ *
+ * @param accessTokenResponse The access token response.
+ */
+export function decodeIdToken(accessTokenResponse: OpenIDAccessTokenResponse) {
+
+	if (accessTokenResponse.id_token) {
+
+		if (typeof accessTokenResponse.id_token === 'string') {
+			accessTokenResponse.id_token = decode(accessTokenResponse.id_token) as OpenIDToken | OpenIDTokenWithStandardClaims;
+		}
+
+	} else if (accessTokenResponse.scope) {
+
+		const scopes = accessTokenResponse.scope.split(' ');
+		if (scopes.includes('openid')) {
+			throw new Error('No id_token present');
+		}
+
+	} else {
+		throw new Error('No id_token present');
+	}
+
+}
+
+/**
+ * Determines whether the id token is an OpenID token with standard claims.
+ *
+ * @param token The id token.
+ * @returns A boolean indicating if this id token is an OpenID token with standard claims.
+ */
+export function isOpenIdTokenWithStandardClaims(token?: string | OpenIDToken | OpenIDTokenWithStandardClaims | null): token is OpenIDTokenWithStandardClaims {
+	return (token as OpenIDTokenWithStandardClaims).email !== undefined;
+}
+
+/**
+ * Determines whether the token is a Salesforce access token response.
+ *
+ * @param token The access token response.
+ * @returns A boolean indicating if this token is a Salesforce access token response.
+ */
+export function isOpenIdAccessTokenResponse(token: AccessTokenResponse | OpenIDAccessTokenResponse): token is OpenIDAccessTokenResponse {
+	return (token as OpenIDAccessTokenResponse).id_token !== undefined;
+}

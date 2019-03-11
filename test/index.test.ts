73cdd63d0ee27c5ed96994531b72da3926350278
Matthew Wood <mwood@financialforce.com>
2019-03-11 11:01:30 +0000

    Update the index Remove all the interfaces that have been moved out and make sure they are exported Do not export the clients as they are internal and should be used with the cache

--- test/index.test.ts
+++ test/index.test.ts
@@ -42,6 +42,7 @@ describe('index', () => {
 			'EVENT_DENIED',
 			'EVENT_GRANT_CHECKED',
 			'EVENT_TOKEN_VALIDATED',
+			'EVENT_USER_IDENTITY_RETRIEVED',
 			'ResponseFormat',
 			'flow',
 			'grant',
@@ -64,6 +65,7 @@ describe('index', () => {
 		expect(index.middleware).to.have.keys([
 			'authCallback',
 			'grantChecker',
+			'retrieveIdentityInformation',
 			'tokenValidator'
 		]);
 

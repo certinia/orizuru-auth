3196e96440b29e2a544aa7d081ab404e53d4473a
Matthew Wood <mwood@financialforce.com>
2019-07-16 09:23:39 +0100

    Update the example code Handle errors and add listeners for the error and info events

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,5 +1,30 @@
-// Imports
-import { json, Request, Response, Server } from '@financialforcedev/orizuru';
+/*
+ * Copyright (c) 2019, FinancialForce.com, inc
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ *   are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ * - Neither the name of the FinancialForce.com, inc nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software without
+ *      specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ *  OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { json, NextFunction, Request, Response, Server } from '@financialforcedev/orizuru';
 import { flow } from '@financialforcedev/orizuru-auth';
 import { Transport } from '@financialforcedev/orizuru-transport-rabbitmq';
 import config from 'config';
@@ -13,12 +38,21 @@ function createCertificate(): Promise<CertificateCreationResult> {
 			if (err) {
 				return reject(err);
 			}
-
 			process.stdout.write('Created certificate\n');
 			return resolve(result);
 		});
 	});
 }
+
+// Define a simple error middleware
+const errorMiddleware = (error: Error | undefined, req: Request, res: Response, next: NextFunction) => {
+	if (error) {
+		server.error(error);
+		res.sendStatus(401);
+	} else {
+		next();
+	}
+};
 
 // Create the server
 const server = new Server({
@@ -36,11 +70,21 @@ const server = new Server({
 	})
 });
 
+// Add listeners for the server error and info events
+server.on(Server.ERROR, (message) => {
+	process.stdout.write(`${message}\n`);
+});
+
+server.on(Server.INFO, (message) => {
+	process.stdout.write(`${message}\n`);
+});
+
 // Add the route to generate the authorization URL (in this case we use 'test' as the state parameter)
 server.addRoute({
 	method: 'get',
 	middleware: [
-		json()
+		json(),
+		errorMiddleware
 	],
 	responseWriter: () => async (err: Error | undefined, req: Request, res: Response) => {
 		const url = await flow.webServer.authorizationUrlGenerator(server.options.authProvider.salesforce)(server.options.openid.salesforce, server.options.openid.salesforce);
@@ -54,8 +98,6 @@ server.addRoute({
 	},
 	synchronous: true
 });
-
-// **All code specified in the rest of the readme should be added here**
 
 // Create a self-signed certificate and then start the server listening to connections using HTTPS
 createCertificate().then((certificate) => {
